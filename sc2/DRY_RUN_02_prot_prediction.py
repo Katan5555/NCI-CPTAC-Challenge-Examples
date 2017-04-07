48ac879329f91c0094a53d591694fc4efd777bb7
Katan5555 <Katan5555@users.noreply.github.com>
2017-04-07 13:16:14 +0200

    Update DRY_RUN_02_prot_prediction.py

--- sc2/DRY_RUN_02_prot_prediction.py
+++ sc2/DRY_RUN_02_prot_prediction.py
@@ -4,16 +4,14 @@
 Created on Wed Apr  5 20:48:38 2017
 @author: miyang
 """
-
-import os
 from scipy import stats
 import xgboost as xgb
 from xgboost.sklearn import XGBClassifier
 from sklearn import cross_validation
 import pandas as pd
 import numpy as np
-import os.path
 import random
+
 path = "/"
 
 
@@ -22,73 +20,67 @@ name_of_response = 'HGSC_prot'
 name_of_features = 'HGSC_rna'  
 
 ## loading data
-RES = pd.read_csv(path + name_of_response, index_col = 0) ;  RES = RES.transpose()
+RES = pd.read_csv(path + name_of_response, index_col = 0)
 features = pd.read_csv(path + name_of_features, index_col = 0) 
 
-nfolds = 10
- 
-    
+nfolds = 10    
 num_round = 10
-result_prediction = []
-for i in range (0, 5):
-    for t in range (0, 5 ):  ## len(RES.iloc[0,:])
-     
-        pp_by_protein = []
-        kf = cross_validation.KFold(len(RES.iloc[:,1]), n_folds = nfolds, shuffle = True,random_state = random.randint(1, 1000) )
+
+result_prediction = []   ## store result for each protein
+
+for t in range (0, len(RES.iloc[0,:]) ):  
+ 
+    pp_by_protein = []   ## store result for each fold of cross validation
+    kf = cross_validation.KFold(len(RES.iloc[:,1]), n_folds = nfolds, shuffle = True,random_state = random.randint(1, 1000) )
+   
+    for train_index, test_index in kf:
+        
+        features_train, features_test = features.iloc[ train_index  , : ], features.iloc[ test_index , : ]
+        RES_train, RES_test = RES.iloc[ train_index, t ] , RES.iloc[ test_index, t ]
+        
+        ii = np.flatnonzero(np.isfinite( RES_train ))
+        features_train = features_train.iloc[ii, :] ;  RES_train = RES_train[ii] 
+        xgtrain = xgb.DMatrix( features_train , label= RES_train )
        
-        for train_index, test_index in kf:
-            
-            features_train, features_test = features.iloc[ train_index  , : ], features.iloc[ test_index , : ]
-            RES_train, RES_test = RES.iloc[ train_index, t ] , RES.iloc[ test_index, t ]
-            
-            ii = np.flatnonzero(np.isfinite( RES_train ))
-            features_train = features_train.iloc[ii, :] ;  RES_train = RES_train[ii] 
-            xgtrain = xgb.DMatrix( features_train , label= RES_train )
-           
-            ii = np.flatnonzero(np.isfinite( RES_test ))
-            features_test = features_test.iloc[ii, :] ;  RES_test = RES_test[ii] 
-            xgbtest = xgb.DMatrix( features_test , label= RES_test )
-    
-            xgb1 = XGBClassifier(
-                 learning_rate =0.3,
-                 n_estimators=30,
-                 max_depth=4,
-                 min_child_weight=1,
-                 gamma=0,
-                 subsample=0.8,
-                 colsample_bytree=0.8,
-                 objective= 'reg:linear',
-                 scale_pos_weight=1,
-                 seed = random.randint(1, 1000) )
-            
-            xgb_params = xgb1.get_xgb_params()
-            xgbooster = xgb.Booster()
-              
-            bst = xgb.train(xgb_params, xgtrain , num_round)
-            pred = bst.predict(xgbtest)
-                    
-            ### compute pearson correlation by protein ###
-            pcorr = stats.pearsonr( RES_test  , pred ) [0]
-            pp_by_protein.append(pcorr)
-                                   
-        result_prediction.append(np.mean(pp_by_protein))
-##  np.mean(result_prediction)
+        ii = np.flatnonzero(np.isfinite( RES_test ))
+        features_test = features_test.iloc[ii, :] ;  RES_test = RES_test[ii] 
+        xgbtest = xgb.DMatrix( features_test , label= RES_test )
 
+        xgb1 = XGBClassifier(
+             learning_rate =0.3,
+             n_estimators=30,
+             max_depth=4,
+             min_child_weight=1,
+             gamma=0,
+             subsample=0.8,
+             colsample_bytree=0.8,
+             objective= 'reg:linear',
+             scale_pos_weight=1,
+             seed = random.randint(1, 1000) )
+        
+        xgb_params = xgb1.get_xgb_params()
+        xgbooster = xgb.Booster()
+          
+        bst = xgb.train(xgb_params, xgtrain , num_round)
+        pred = bst.predict(xgbtest)
+                
+        ### compute pearson correlation by protein ###
+        pcorr = stats.pearsonr( RES_test  , pred ) [0]
+        pp_by_protein.append(pcorr)
+                               
+    result_prediction.append(np.mean(pp_by_protein))   ## append mean over k fold cross validation
 
-result_prediction= pd.DataFrame(result_prediction)
+result_prediction= pd.DataFrame(result_prediction)     ## np.mean(result_prediction)
 #result_prediction.to_csv(path + 'prediction_result_' + str(random.randint(1, 1000)))
 result_prediction.to_csv(path + 'output/prediction.csv') 
-
+                         
                 
                          
 #######################################################  AVERAGING RESULT FROM MULTIPLE RUNS #######################################################
 
 # prefixed = [filename for filename in os.listdir(path) if filename.startswith('prediction_result')]
-
-# iteration=3
+# iteration=3   ## choose the number of iteration starting from which to average the result
 # if len(prefixed) >= iteration:
-    
-#     prefixed = [filename for filename in os.listdir(path) if filename.startswith('prediction_result')]
 #     frame = pd.DataFrame()
 #     list = []
 #     for file_ in prefixed:
@@ -96,6 +88,3 @@ result_prediction.to_csv(path + 'output/prediction.csv')
 #         list.append(df)
 #     mean = pd.concat([each.stack() for each in list],axis=1).apply(lambda x:x.mean(),axis=1).unstack() ; mean = mean.iloc[ : , 1 ]
 #     mean.to_csv(path + '/prediction_result'+'_'+str(iteration)+'ite')
-
-
-
