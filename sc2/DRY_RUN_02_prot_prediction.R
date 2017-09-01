788ea3f4a192638dbd63149b7a669ffd217868e4
Katan5555 <Katan5555@users.noreply.github.com>
2017-09-01 13:31:19 +0200

    Update DRY_RUN_02_prot_prediction.R

--- sc2/DRY_RUN_02_prot_prediction.R
+++ sc2/DRY_RUN_02_prot_prediction.R
@@ -2,36 +2,33 @@
 # path <- "/Users/miyang/Documents/RWTH_Aachen/DREAM_CPTAC/DOCKER_STORAGE/docker_R_sc2/"
 path <- "/"
 
+# load package and functions
+library(glmnet)
+predictEN <- function(model, lambda, feat) {
+  return(predict(model,type="response",newx=feat)[,model$lambda == lambda])
+}
+
 # load saved models
-load(paste0(path,"model_storage/SAVE_WEIGHT_HGSC_ALL_PROT_RNA_microarray_fold10_ite10.Rdata"))
+load(paste0(path,"model_storage/SAVE_MODEL_ElasticNet_HGSC_ALL_PROT_microarray_fold5_ite10.Rdata"))
 
 # load testing data
 HGSC_rna_EVAL  <- read.csv(paste0(path,"evaluation_data/prospective_ova_rna_seq_sort_common_gene_15632.txt"), row.names= 1, sep="\t", check.names = F )
-HGSC_prot_EVAL <- read.csv(paste0(path,"evaluation_data/prospective_ova_proteome_filtered.txt"), row.names= 1, sep="\t", check.names = F )
+features <- t(HGSC_rna_EVAL)
 
-# take common patients
-common_patient <- intersect(colnames(HGSC_prot_EVAL),colnames(HGSC_rna_EVAL))
-HGSC_prot_EVAL <- HGSC_prot_EVAL[ ,common_patient] ; HGSC_rna_EVAL <- HGSC_rna_EVAL[ ,common_patient]
+prediction <- c()
+for(i in 1:length(model)) {
+  fit <- model[[i]]
+  test_pred <- predictEN(fit,fit$lambda, features ) 
+  prediction <- rbind(prediction, test_pred)
+}
+rownames(prediction) <- names(model)
 
-# take common proteins and common predictors
-common_protein <- intersect(rownames(weight), rownames(HGSC_prot_EVAL))
-weight <- weight[common_protein, ]
-HGSC_prot_EVAL <- HGSC_prot_EVAL[ common_protein , ]
-
-common_feature <- intersect(colnames(weight), rownames(HGSC_rna_EVAL))
-weight <- weight[ , common_feature]
-HGSC_rna_EVAL <- HGSC_rna_EVAL[ common_feature , ]
-
-# matrix multiplication to make the prediction
-weight <- as.matrix(weight) ; HGSC_rna_EVAL <- as.matrix(HGSC_rna_EVAL)
-prediction_ovarian <- weight %*% HGSC_rna_EVAL
-
-prediction_ovarian[which(rowSums(prediction_ovarian)==0), 1] <- 0.1
-prediction_ovarian <- cbind(rownames(prediction_ovarian),prediction_ovarian)
-colnames(prediction_ovarian)[1] <- "proteinID"
+# prediction[which(apply(prediction, 1, var) == 0),  ] <- features[names(which(apply(prediction, 1, var) == 0)), ]
+prediction <- cbind(proteinID = rownames(prediction),prediction)
 
 # save the prediction matrix
-write.table(prediction_ovarian, file = paste0(path,"output/predictions.tsv"), sep="\t" )
+write.table(prediction, file = paste0(path,"output/predictions.tsv"), sep="\t" )
 
 # save the confidence matrix 
-write.table(prediction_ovarian, file = paste0(path,"output/confidence.tsv"), sep="\t" )
+write.table(prediction, file = paste0(path,"output/confidence.tsv"), sep="\t" )
+
